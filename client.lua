ffaf2436df59aa2e6b1cec46941ad50673d08cf5
Pawan Paudel <pawanpaudel93@gmail.com>
2024-02-25 23:47:00 +0545

    Initial Commit

---
+++ client.lua
@@ -0,0 +1,131 @@
+local json = require("json")
+
+local GameProcess = "DmwzscUB2NejQ9PZpRSaVhYRtgY6wV8_ffmGyJjWmZA"
+
+-- Function to print the current state of the board
+function printBoard(board)
+    for i = 1, #board, 3 do
+        print(board[i] .. " | " .. board[i + 1] .. " | " .. board[i + 2])
+        if i < 7 then
+            print("---------")
+        end
+    end
+    print() -- New line for better spacing
+end
+
+function register()
+    Send({ Target = GameProcess, Action = "Register" })
+end
+
+function makeMove(position)
+    Send({ Target = GameProcess, Action = "MakeMove", Position = position })
+end
+
+Handlers.add(
+    "GameState",
+    Handlers.utils.hasMatchingTag("Action", "GameState"),
+    function(msg)
+        local Data = json.decode(msg.Data)
+        local playerSymbol = Data.Players[ao.id]
+        if (playerSymbol ~= nil) then
+            Prompt = function()
+                return "Player[" .. playerSymbol .. "]@aos> "
+            end
+            printBoard(Data.Board)
+            if (Data.CurrentPlayer == ao.id) then
+                print("\nIt's your turn. Choose a position (1-9):\n")
+            else
+                print("\nIt's your opponent turn.\n")
+            end
+        end
+    end
+)
+
+Handlers.add(
+    "Registered",
+    Handlers.utils.hasMatchingTag("Action", "Registered"),
+    function(msg)
+        print("You have registered to the game. Your symbol is " .. msg.Tags.Symbol .. "\n")
+        Prompt = function()
+            return "Player[" .. msg.Tags.Symbol .. "]@aos> "
+        end
+    end
+)
+
+Handlers.add(
+    "Register-Error",
+    Handlers.utils.hasMatchingTag("Action", "Register-Error"),
+    function(msg)
+        print(msg.Tags.Error .. "\n")
+    end
+)
+
+Handlers.add(
+    "Position-Error",
+    Handlers.utils.hasMatchingTag("Action", "Position-Error"),
+    function(msg)
+        print(msg.Tags.Error .. "\n")
+    end
+)
+
+Handlers.add(
+    "Winner",
+    Handlers.utils.hasMatchingTag("Action", "Winner"),
+    function(msg)
+        local Data = json.decode(msg.Data)
+        printBoard(Data.Board)
+        if (ao.id == msg.Tags.Winner) then
+            print("\nYou have won the game.\n")
+        else
+            print("\nYou have lost the game.\n")
+        end
+    end
+)
+
+Handlers.add(
+    "Play",
+    Handlers.utils.hasMatchingTag("Action", "Play"),
+    function(msg)
+        printBoard({ "1", "2", "3", "4", "5", "6", "7", "8", "9" })
+        print("\nThe game has started.\n")
+        if (msg.Tags.CurrentPlayer == ao.id) then
+            print("It's your turn. Choose a position (1-9):\n")
+        else
+            print("It's your opponent turn.\n")
+        end
+    end
+)
+
+Handlers.add(
+    "Draw",
+    Handlers.utils.hasMatchingTag("Action", "Draw"),
+    function(msg)
+        local Data = json.decode(msg.Data)
+        printBoard(Data.Board)
+        print("\nThe game has ended in a Draw.\n")
+    end
+)
+
+Handlers.add(
+    "Register-Error",
+    Handlers.utils.hasMatchingTag("Action", "Draw"),
+    function(msg)
+        print("Game ended with a Draw.\n")
+    end
+)
+
+Handlers.add(
+    "CurrentTurn",
+    Handlers.utils.hasMatchingTag("Action", "CurrentTurn"),
+    function(msg)
+        local Data = json.decode(msg.Data)
+        printBoard(Data.Board)
+        if (msg.Tags.CurrentPlayer == ao.id) then
+            print("\nIt's your turn. Choose a position (1-9):\n")
+        else
+            print("\nIt's your opponent turn.\n")
+        end
+    end
+)
+
+Send({ Target = GameProcess, Action = "GetGameState" })
---
+++ tictactoe.lua
@@ -0,0 +1,149 @@
+local json = require("json")
+
+-- Initialize the Board as a single-dimensional array for simplicity
+Board = Board or { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+Players = Players or {}
+CurrentPlayer = CurrentPlayer or ""
+State = State or "REGISTER"
+
+
+function resetState()
+    Board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
+    Players = {}
+    CurrentPlayer = ""
+    State = "REGISTER"
+end
+
+function informPlayers(message)
+    for address, _ in pairs(Players) do
+        message.Target = address
+        ao.send(message)
+    end
+end
+
+-- Function to check if the current player has won
+function checkWin()
+    local wins = {
+        { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, -- Rows
+        { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 }, -- Columns
+        { 1, 5, 9 }, { 3, 5, 7 }               -- Diagonals
+    }
+
+    for _, win in ipairs(wins) do
+        if Board[win[1]] == Board[win[2]] and Board[win[2]] == Board[win[3]] then
+            return true
+        end
+    end
+    return false
+end
+
+-- Function to check if the Board is full
+function checkDraw()
+    for _, value in ipairs(Board) do
+        if value ~= "X" and value ~= "O" then
+            return false
+        end
+    end
+    return true
+end
+
+-- Function to update the Board with the player's move
+function movePlayer(player, position)
+    if position and position >= 1 and position <= 9 and Board[position] ~= "X" and Board[position] ~= "O" then
+        Board[position] = player
+        return true
+    end
+    return false
+end
+
+function getPlayersCount()
+    local count = 0
+    for _, _ in pairs(Players) do
+        count = count + 1
+    end
+    return count
+end
+
+function register(msg)
+    local playersCount = getPlayersCount()
+    assert(State == "REGISTER", "Game is already started!")
+    assert(Players[msg.From] == nil, "Player already registered!")
+    assert(playersCount < 2, "Game already full!")
+
+
+    if (playersCount <= 2) then
+        local symbol = getPlayersCount() == 0 and "X" or "O"
+        Players[msg.From] = symbol
+
+        if CurrentPlayer == "" then CurrentPlayer = msg.From end
+
+        ao.send({
+            Target = msg.From,
+            Action = "Registered",
+            Symbol = symbol
+        })
+
+        if (getPlayersCount() == 2) then
+            State = "PLAY"
+            informPlayers({ Action = "Play", CurrentPlayer = CurrentPlayer })
+        end
+    else
+        ao.send({
+            Target = msg.From,
+            Action = "Register-Error",
+            ["Message-Id"] = msg.id,
+            Error = "Game already occupied!"
+        })
+    end
+end
+
+function makeMove(msg)
+    assert(type(msg.Tags.Position) == 'string', 'Position is required!')
+    assert(Players[msg.From] ~= nil, "Player is not registered to play!")
+    assert(getPlayersCount() == 2, "Two players are not registered to play!")
+    assert(msg.From == CurrentPlayer, "This is not your turn!")
+    assert(State == "PLAY", "Game is not started or already completed!")
+
+    local position = tonumber(msg.Tags.Position)
+    local playerSymbol = Players[msg.From]
+
+    if movePlayer(playerSymbol, position) then
+        if checkWin() then
+            informPlayers({ Action = "Winner", Winner = msg.From, Data = json.encode({ Board = Board }) })
+            resetState()
+        elseif checkDraw() then
+            informPlayers({ Action = "Draw", Data = json.encode({ Board = Board }) })
+            resetState()
+        else
+            for address, _ in pairs(Players) do
+                if address ~= msg.From then CurrentPlayer = address end
+            end
+            informPlayers({ Action = "CurrentTurn", Data = json.encode({ Board = Board }), CurrentPlayer = CurrentPlayer })
+        end
+    else
+        ao.send({
+            Target = msg.From,
+            Action = "Position-Error",
+            ["Message-Id"] = msg.id,
+            Error = "Invalid position, try again"
+        })
+    end
+end
+
+function getGameState(msg)
+    local GameState = json.encode({
+        State = State,
+        Board = Board,
+        Players = Players,
+        CurrentPlayer = CurrentPlayer
+    })
+    ao.send({
+        Target = msg.From,
+        Action = "GameState",
+        Data = GameState
+    })
+end
+
+Handlers.add("Register", Handlers.utils.hasMatchingTag("Action", "Register"), register)
+Handlers.add("MakeMove", Handlers.utils.hasMatchingTag("Action", "MakeMove"), makeMove)
+Handlers.add("GetGameState", Handlers.utils.hasMatchingTag("Action", "GetGameState"), getGameState)
